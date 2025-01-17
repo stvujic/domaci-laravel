f95cbb190c3432ef60a31046390d7dbdac709087
stvujic <83906734+stvujic@users.noreply.github.com>
2025-01-17 13:21:46 +0100

    * Route prefix * Route group

--- routes/web.php
+++ routes/web.php
@@ -12,32 +12,35 @@ Route::get("/", [HomepageController::class, "index"]);
 Route::get("/shop", [ShopController::class, "index"]);
 Route::get("/contact", [ContactController::class, "index"]);
 
-Route::get("/admin/all-contacts", [ContactController::class, "getAllContacts"]);
-Route::get("/admin/all-products", [ProductsController::class, "index"])
-    ->name("sviProizvodi");
+Route::middleware("auth")->prefix("admin")->group(function () {
+
+    Route::get("/all-contacts", [ContactController::class, "getAllContacts"]);
+    Route::get("/all-products", [ProductsController::class, "index"])
+        ->middleware("auth")
+        ->name("sviProizvodi");
 
 
-Route::get("/admin/delete-product/{product}", [ProductsController::class, "delete"])
-    ->name("obrisiProizvod");
-Route::get("/admin/delete-contact/{contact}", [ContactController::class, "delete"])
-    ->name("obrisiKontakt");
+    Route::get("/delete-product/{product}", [ProductsController::class, "delete"])
+        ->name("obrisiProizvod");
+    Route::get("/delete-contact/{contact}", [ContactController::class, "delete"])
+        ->name("obrisiKontakt");
 
 
-Route::view("/admin/add-product", "addProduct");
+    Route::view("/add-product", "addProduct");
 
-Route::post("/send-contact", [ContactController::class, "sendContact"]);
-Route::post("/admin/save-product", [ProductsController::class, "saveProduct"])
-    ->name("snimanjeOglasa");
+    Route::post("/send-contact", [ContactController::class, "sendContact"]);
+    Route::post("/save-product", [ProductsController::class, "saveProduct"])
+        ->name("snimanjeOglasa");
 
-Route::get("/admin/product/edit/{product}", [ProductsController::class, "singleProduct"])
-    ->name("product.single");
-Route::post("/admin/product/save/{product}", [ProductsController::class, "edit"])
-    ->name("product.save");
+    Route::get("/product/edit/{product}", [ProductsController::class, "singleProduct"])
+        ->name("product.single");
+    Route::post("/product/save/{product}", [ProductsController::class, "edit"])
+        ->name("product.save");
 
-Route::get("/admin/contact/edit/{id}", [ContactController::class, "singleContact"])
-    ->name("contact.single");
-Route::post("/admin/contact/save/{id}", [ContactController::class, "edit"])
-    ->name("contact.save");
-
+    Route::get("/contact/edit/{id}", [ContactController::class, "singleContact"])
+        ->name("contact.single");
+    Route::post("/contact/save/{id}", [ContactController::class, "edit"])
+        ->name("contact.save");
+});
 
 require __DIR__.'/auth.php';
